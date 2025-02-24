34af200bfdd81b3be0ebce4eebd6fcc999d80d16
Jesús Mira <jesmira@hotmail.es>
2025-02-24 10:54:21 +0100

    Fin de la sección 12

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { envs } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+
+  // TODO: Instalación de NATS, @NestJS/microservices
+  // PORT: 3004 configuracion variables de entorno
+  const logger = new Logger('Auth Microservice');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers,
+      },
+    }
+  );
+
+  app.useGlobalPipes( 
+    new ValidationPipe({ 
+      whitelist: true, 
+      forbidNonWhitelisted: true, 
+    }) 
+  );
+
+  
+  await app.listen();
+  logger.log(`Auth Microservice is running on ${envs.port}`);
 }
 bootstrap();
